48896a748de8b009f41403427749f15a58aa0f0f
logan <gnlee95@gmail.com>
2022-03-23 23:44:33 +0900

    Add DTO for create boards

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -11,7 +12,8 @@ export class BoardsService {
   }
 
   // Tip: Unique ID 줄때, MongoDB 등에서 알아서 지정해주지만 일단은 uuid 모듈 사용
-  createBoard(title: string, description: string) {
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
     const board: Board = {
       id: uuid(),
       title,
