4d592f949fd8808211590fd900dc1bea00f8b041
logan <gnlee95@gmail.com>
2022-04-03 12:35:03 +0900

    Implements delete function

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
-// Get에서 URL query string을 @Param()으로 받음
-// Post에서 Body를 @Body()로 받음
-// 이때 전체를 가져오려면 괄호를 공백으로
-// 특정 element를 가져오려면 괄호 안에 element 요소를 써줌
-// 받아온 요소를 뒤의 변수에 넣어줌
+// - Get에서 URL query string을 @Param()으로 받음
+// - Post에서 Body를 @Body()로 받음
+//    이때 전체를 가져오려면 괄호를 공백으로
+//    특정 element를 가져오려면 괄호 안에 element 요소를 써줌
+//    받아온 요소를 뒤의 변수에 넣어줌
+// - Delete에서 return하는 것 없이 단순히 삭제만 한다면
+//    Service는 void이고 Controller도 Service 실행만 하고 void
 
 @Controller('boards')
 export class BoardsController {
@@ -26,5 +28,10 @@ export class BoardsController {
   @Post()
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
   }
 }
