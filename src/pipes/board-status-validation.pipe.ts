7d0e676e1059cb70eb1d3b552b44a6e95eaec67d
logan <gnlee95@gmail.com>
2022-04-25 17:41:46 +0900

    Feat: Add custom pipe

--- src/pipes/board-status-validation.pipe.ts
+++ src/pipes/board-status-validation.pipe.ts
@@ -1,10 +1,20 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../boards/board-status.enum';
 
 export class BoardStatusValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    console.log('value');
-    console.log('metadata', metadata);
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
 
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status`);
+    }
     return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
   }
 }
