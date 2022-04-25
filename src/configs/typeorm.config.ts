22e8fd09d0e6a3119e9c7a5878a9434a431daf10
logan <gnlee95@gmail.com>
2022-04-25 15:53:19 +0900

    Add TypeOrm

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'boardproject',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'], // 개별 엔티티를 하나씩 넣을 수도 있음
+  synchronize: true, // production 모드에서는 false로 설정 (데이터 손실 가능성 존재)
+};
