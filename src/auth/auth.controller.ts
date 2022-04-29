9b3ddd4bf34fa53ab6fda0323b5b39da7093b688
logan <gnlee95@gmail.com>
2022-04-29 14:38:00 +0900

    Update signup to add validation check

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
@@ -6,8 +6,11 @@ import { AuthCredentialsDto } from './dto/auth-credential.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  // 요청이 컨트롤러로 들어올 때 Dto에 잇는 유효성 조건에 맞게 체크를 해주려면 ValidationPipe를 추가!!!
   @Post('/signup')
-  signUp(@Body() authcredentialsDto: AuthCredentialsDto): Promise<void> {
+  signUp(
+    @Body(ValidationPipe) authcredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
     return this.authService.signUp(authcredentialsDto);
   }
 }
