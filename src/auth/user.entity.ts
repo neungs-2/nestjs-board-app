4d2874fa61976a20e5021de232aeaf3cb8121f9a
logan <gnlee95@gmail.com>
2022-04-29 16:27:05 +0900

    Update dto and repo to apply unique ID

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,16 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
+// 유저 이름에 유니크한 값 주기!
+// 방법 1: repository에서 findOne 이용하여 같은 이름의 데이터 확인 후 저장 (DB 조회, 삽입 2번의 트랜잭션)
+// 방법 2: DB 레벨에서 같은 이름 존재 시 에러를 반환 (DB에 그냥 삽입하는 트랜잭션만 발생)
 @Entity()
+@Unique(['username']) // 위의 방법 2에 해당하는 코드
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
