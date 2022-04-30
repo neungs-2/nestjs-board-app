afefbdcf57d8e2023091c24602a48717a2400319
logan <gnlee95@gmail.com>
2022-04-30 15:37:13 +0900

    feat: Add jwt and passport to create access token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,14 +8,12 @@ export class AuthController {
 
   // 요청이 컨트롤러로 들어올 때 Dto에 잇는 유효성 조건에 맞게 체크를 해주려면 ValidationPipe를 추가!!!
   @Post('/signup')
-  signUp(
-    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
-  ): Promise<void> {
+  signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
 
   @Post('/signin')
-  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
 }
