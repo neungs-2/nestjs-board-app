1093e40c91d6be3a713b949ec9b74a5c5daff39e
logan <gnlee95@gmail.com>
2022-05-01 01:29:27 +0900

    feat: Implement passport-jwt authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
@@ -15,5 +16,20 @@ export class AuthController {
   @Post('/signin')
   signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
+  }
+
+  // 테스트 결과 요청 내부에 user 객체가 들어있지 않음
+  @Post('/test')
+  test(@Req() req) {
+    console.log('req', req);
+  }
+
+  // MEMO: 요청 안에 유저 정보(유저 객체)가 들어가게 하는 방법
+  // UseGuards 안에 @nestjs/passport에서 가져온 AuthGuard()를 사용하여 넣어줌
+  // Guard는 인증을 위한 미들웨어!
+  @Post('/authTest')
+  @UseGuards(AuthGuard())
+  authTest(@Req() req) {
+    console.log('req', req);
   }
 }
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+
+@Injectable() // JwtStrategy를 다른 모듈에서도 의존성을 주입하여 사용할 수 있게 하기 위한 Decorator
+// passport-jwt를 사용하기 위해서 인자로 Strategy를 받음
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {
+    super({
+      secretOrkey: 'SecretKey',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload) {
+    const { username } = payload;
+    const user: User = await this.userRepository.findOne({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
